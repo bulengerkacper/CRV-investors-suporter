99811487cd15d3affe37dc2c9ae2d3dfef1d53de
bulengerkacper <bulenger.kacper@gmail.com>
2021-12-30 13:05:56 +0100

    first little bug occured

--- app.py
+++ app.py
@@ -23,3 +23,7 @@ def compare_15min_to_3days():
 @app.route("/compare_15min_to_5days")
 def compare_15min_to_5days():
     return str(scraper.compare_15min_to_5days())
+
+@app.route("/get_current_value")
+def get_current_value():
+    return str(scraper.get_current_value())
--- crv.py
+++ crv.py
@@ -18,41 +18,46 @@ class Scrapper:
         self.current_day=datetime.now().day
         self.start_long_term = dt.datetime(2020, 8, 14) 
         self.end_long_term = dt.datetime(self.current_year,self.current_month,self.current_day)
-        self.crv = web.DataReader("CRV-USD", 'yahoo', self.start_long_term, self.end_long_term)  # Collects data
+        self.crv = web.DataReader("CRV-USD", 'yahoo', self.start_long_term, self.end_long_term)
 
     def avg_from_days(self,how_many_elements):
         sum=0
         only_adj_close=self.crv["Adj Close"]
         for rest in only_adj_close[-how_many_elements:]:
             sum=sum+rest
-        sum=sum/how_many_elements
-        return sum
+        return round(sum/how_many_elements,4)
 
     def get_avg_from_last_15min(self):
         sum=0
-        counter=0
+        how_many_elements=0
         data = yf.download(tickers='CRV-USD', period = '15m', interval = '1m')
         for rest in data['Close']:
             sum=sum+rest
-            counter+=1
-        avg=sum/counter
-        return avg
+            how_many_elements+=1
+        return round(sum/how_many_elements,4)
+    
+    def get_current_value(self):
+        data=yf.download(tickers='CRV-USD', period = '15m', interval = '1m')
+        amount=data['Close']
+        result=amount[-1:]
+        print(result)
+        return result
     
     def compare_15min_to_3days(self):
         from_last_15min=self.get_avg_from_last_15min()
         avg_from_3_days=self.avg_from_days(3)
         if (from_last_15min < avg_from_3_days):
-            return "<span class='"'nes-text is-success'"'>Avg from last 15 min is < avg from 3 days</span>"
+            return "<span class='"'nes-text is-success'"'>Avg from last 15 min < 3 days</span>"
         else:
-            return "<span class='"'nes-text is-error'"'>Avg from last 15 min is > avg from 3 days</span>"
+            return "<span class='"'nes-text is-error'"'>Avg from last 15 min > 3 days</span>"
 
     def compare_15min_to_5days(self):
         from_last_15min=self.get_avg_from_last_15min()
         avg_from_5_days=self.avg_from_days(5)
         if (from_last_15min < avg_from_5_days):
-            return "<span class='"'nes-text is-success'"'>Avg from last 15 min is < avg from 5 days</span>"
+            return "<span class='"'nes-text is-success'"'>Avg from last 15 min < 5 days</span>"
         else:
-            return "<span class='"'nes-text is-error'"'>Avg from last 15 min is > avg from 5 days</span>"
+            return "<span class='"'nes-text is-error'"'>Avg from last 15 min > 5 days</span>"
 
 #data scraper
 ##################
