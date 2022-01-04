118220b90e0ad2223f81120c079023b7af798f43
bulengerkacper <bulenger.kacper@gmail.com>
2022-01-04 22:29:37 +0100

    white spacing

--- src/static/calc.js
+++ src/static/calc.js
@@ -1,75 +1,78 @@
 $.get('/avg/3',  // url
-function (data, textStatus, jqXHR) {  // success callback
-    // alert('status: ' + textStatus + ', data:' + data);
-    $('result_of_avg_3').append("3 DAYS: " + data + "</br>");
-});
+    function (data, textStatus, jqXHR) {  // success callback
+        // alert('status: ' + textStatus + ', data:' + data);
+        $('result_of_avg_3').append("3 DAYS: " + data + "</br>");
+    });
 $.get('/avg/5',  // url
-function (data, textStatus, jqXHR) {  // success callback
-// alert('status: ' + textStatus + ', data:' + data);
-$('result_of_avg_5').append("5 DAYS: " + data + "</br>");
+    function (data, textStatus, jqXHR) {  // success callback
+    // alert('status: ' + textStatus + ', data:' + data);
+    $('result_of_avg_5').append("5 DAYS: " + data + "</br>");
 });
 $.get('/avg/31',  // url
-function (data, textStatus, jqXHR) {  // success callback
-// alert('status: ' + textStatus + ', data:' + data);
-$('result_of_avg_31').append("31 DAYS: " + data + "</br>");
+    function (data, textStatus, jqXHR) {  // success callback
+    // alert('status: ' + textStatus + ', data:' + data);
+    $('result_of_avg_31').append("31 DAYS: " + data + "</br>");
 });
 $.get('/avg/180',  // url
-function (data, textStatus, jqXHR) {  // success callback
-// alert('status: ' + textStatus + ', data:' + data);
-$('result_of_avg_180').append("180 DAYS: " + data + "</br>");
+    function (data, textStatus, jqXHR) {  // success callback
+    // alert('status: ' + textStatus + ', data:' + data);
+    $('result_of_avg_180').append("180 DAYS: " + data + "</br>");
 });
 
 $.get('/avg_from_last_15min',  // url
-function (data, textStatus, jqXHR) {  // success callback
-// alert('status: ' + textStatus + ', data:' + data);
-$('result_of_avg_15min').append("15 min: " + data + "</br>");
+    function (data, textStatus, jqXHR) {  // success callback
+    // alert('status: ' + textStatus + ', data:' + data);
+    $('result_of_avg_15min').append("15 min: " + data + "</br>");
 });
 $.get('/compare_15min_to_days/3',  // url
-function (data, textStatus, jqXHR) {  // success callback
-// alert('status: ' + textStatus + ', data:' + data);
-$('compare_to_3').append(data);
+    function (data, textStatus, jqXHR) {  // success callback
+    // alert('status: ' + textStatus + ', data:' + data);
+    $('compare_to_3').append(data);
 });
 $.get('/compare_15min_to_days/5',  // url
-function (data, textStatus, jqXHR) {  // success callback
-// alert('status: ' + textStatus + ', data:' + data);
-$('compare_to_5').append(data);
+    function (data, textStatus, jqXHR) {  // success callback
+    // alert('status: ' + textStatus + ', data:' + data);
+    $('compare_to_5').append(data);
 });
 $.get('/compare_15min_to_days/31',  // url
-function (data, textStatus, jqXHR) {  // success callback
-// alert('status: ' + textStatus + ', data:' + data);
-$('compare_to_31').append(data);
+    function (data, textStatus, jqXHR) {  // success callback
+    // alert('status: ' + textStatus + ', data:' + data);
+    $('compare_to_31').append(data);
 });
 $.get('/compare_15min_to_days/180',  // url
-function (data, textStatus, jqXHR) {  // success callback
-// alert('status: ' + textStatus + ', data:' + data);
-$('compare_to_180').append(data);
-});
+    function (data, textStatus, jqXHR) {  // success callback
+    // alert('status: ' + textStatus + ', data:' + data);
+    $('compare_to_180').append(data);
+    });
+
 $.get('/get_current_value',  // url
-function (data, textStatus, jqXHR) {  // success callback
-// alert('status: ' + textStatus + ', data:' + data);
-$('cur_val').append(data);
+    function (data, textStatus, jqXHR) {  // success callback
+    // alert('status: ' + textStatus + ', data:' + data);
+    $('cur_val').append(data);
 });
+
 $.get('/yesterday_to_today',  // url
-function (data, textStatus, jqXHR) {  // success callback
-// take wrong value after refresh
-$('yesterday_to_today').append(data);
+    function (data, textStatus, jqXHR) {  // success callback
+    // take wrong value after refresh
+    $('yesterday_to_today').append(data);
 });
+
 $.get('/rsi/14', 
-function (data, textStatus, jqXHR) {  // success callback
-// take wrong value after refresh
-$('rsi_14').append(data);
+    function (data, textStatus, jqXHR) {  // success callback
+    // take wrong value after refresh
+    $('rsi_14').append(data);
 });
 function myFunction() {
-coin_type=document.getElementById("success_select").value;
-$.ajax({
-type: 'POST',
-contentType: 'application/json',
-url: '/coinswitcher',
-dataType: 'json',
-data: JSON.stringify({ "value": coin_type }),
-success: function (result) {
-},
-error: function (error) { console.error(error) }
-});
-location.reload();
+    coin_type=document.getElementById("success_select").value;
+    $.ajax({
+    type: 'POST',
+    contentType: 'application/json',
+    url: '/coinswitcher',
+    dataType: 'json',
+    data: JSON.stringify({ "value": coin_type }),
+    success: function (result) {
+    },
+    error: function (error) { console.error(error) }
+    });
+    location.reload();
 }
